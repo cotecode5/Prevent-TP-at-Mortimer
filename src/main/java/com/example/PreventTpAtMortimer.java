097bcdae9ba8903c2c686f406b49e1cb26983bda
Dr4c0 <si902.scote@gmail.com>
2026-09-23 00:13:58 -0400

    Update PreventTpAtMortimer.java

--- src/main/java/com/example/PreventTpAtMortimer.java
+++ src/main/java/com/example/PreventTpAtMortimer.java
@@ -10,10 +10,20 @@ public interface PreventTpAtMortimer extends Config
     @ConfigItem(
         keyName = "enableGuard",
         name = "Enable Teleport Guard",
-        description = "Blocks all item/cape teleports inside Wyrmscraig Cavern to save boat placement"
+        description = "Blocks teleport and travel actions inside Wyrmscraig Cavern"
     )
     default boolean enableGuard()
     {
         return true;
+    }
+
+    @ConfigItem(
+        keyName = "debugLogging",
+        name = "Enable Debug Logging",
+        description = "Logs detailed menu and widget information while inside Wyrmscraig Cavern"
+    )
+    default boolean debugLogging()
+    {
+        return false;
     }
 }
